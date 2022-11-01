010df2f72da1ad216478c8392d207a446b88426b
fingacode <junior@kidindustry.co.uk>
2022-11-01 04:41:05 +0000

    resize scribbles

--- src/shapes/scribbles.js
+++ src/shapes/scribbles.js
@@ -21,74 +21,91 @@ export class Scribbles {
     this.isOverstitch = isOverstitch;
     this.isGlitch = isGlitch;
 
-    // Store
-    this.isLineVertical = R.random_bool(0.5);
+    // Props
+    this.isVertical = R.random_bool(0.5);
+    this.lineSize = this.isVertical ? this.height : this.width;
+    this.containerSize = this.isVertical ? this.width : this.height;
     this.curveSize = R.random_int(2, 8);
+    this.lineCount = Math.ceil(this.containerSize / this.curveSize) - 1;
+    this.primaryCol = pickRndColor(this.palette);
+    this.secondaryColArr = [];
 
-    this.thicknessArr = [];
+    this.toggleRotInit = R.random_bool(0.5);
     this.toggleRotArr = [];
-    this.dividersArr = [];
+
+    this.dividerArr = [];
+
+    this.lineThickArr = [];
     this.offsetArr = [];
     this.curveTypeArr = [];
-
+    this.noiseArr = [];
   }
 
-  show(drawScale = 1, redraw = false) {
+  generate() {
+    const QUARTER_PI = 0.7853982;
+    const HALF_PI = QUARTER_PI * 2;
+    const PI = HALF_PI * 2;
 
-    console.log(drawScale)
+    for (let l = 0; l < this.lineCount; l++) {
+      this.lineThickArr.push(R.random_int(1, 3));
+
+      if (this.isGlitch) {
+        this.toggleRotArr.push(R.random_bool(0.5));
+        this.dividerArr.push(R.random_int(1, 100));
+        this.secondaryColArr.push(pickRndColor(this.palette));
+      }
+
+      const offset = R.random_int(1, 10);
+      this.offsetArr.push(offset);
+      this.curveTypeArr.push(R.random_choice([QUARTER_PI, HALF_PI, PI]));
+
+      this.noiseArr.push([]);
+
+      for (let i = offset; i < this.lineSize - offset; i += 1) {
+        const noise = this.isNoisy ? R.random_dec() : 0;
+        this.noiseArr[l].push(noise);
+      }
+    }
+  }
+
+  show(drawScale = 1) {
     noFill();
 
-    const col = pickRndColor(this.palette);
+    const col = this.primaryCol;
     stroke(color(col.r, col.g, col.b));
 
-    const containerSize = this.isLineVertical
-      ? this.width * drawScale
-      : this.height * drawScale;
-    const lineCountOffset = 1;
-    const lineCount =
-      Math.ceil(containerSize / (this.curveSize * drawScale)) - lineCountOffset;
-
     let lineTranslate = this.curveSize;
-    let toggleRot = R.random_bool(0.5);
-    let divider = 100;
 
     push();
 
-    for (let l = 0; l < lineCount; l++) {
-      const thickness = R.random_int(1, 3);
-      strokeWeight(thickness);
+    for (let l = 0; l < this.lineCount; l++) {
+      strokeWeight(this.lineThickArr[l] * drawScale);
 
-      if (this.isLineVertical) {
-        translate(lineTranslate, 0);
+      if (this.isVertical) {
+        translate(lineTranslate * drawScale, 0);
       } else {
-        translate(0, lineTranslate);
+        translate(0, lineTranslate * drawScale);
       }
 
       if (this.isGlitch) {
-        toggleRot = R.random_bool(0.5);
-        divider = R.random_int(1, 100);
-
-        const rotation = radians(l / divider);
-        const newRotation = toggleRot ? -rotation : rotation;
+        const rotation = radians(l / this.dividerArr[l]);
+        const newRotation = this.toggleRotArr[l] ? -rotation : rotation;
         rotate(newRotation);
 
-        const col = pickRndColor(this.palette);
+        const col = this.secondaryColArr[l];
         stroke(color(col.r, col.g, col.b));
       }
 
-      // SAVE
-      this.toggleRotArr.push(toggleRot);
-
-      const offset = R.random_num(1, 10);
-      const curveType = R.random_choice([QUARTER_PI, HALF_PI, PI]);
+      const offset = this.offsetArr[l];
+      const curveType = this.curveTypeArr[l];
 
       beginShape();
 
-      for (let i = offset; i < containerSize - offset; i += 1) {
+      for (let i = offset; i < this.lineSize - offset; i += 1) {
         let x, y;
-        let noise = this.isNoisy ? R.random_dec() : 0;
+        let noise = this.noiseArr[l][i];
 
-        if (this.isLineVertical) {
+        if (this.isVertical) {
           if (this.isOverstitch && !this.isCascade) {
             x = tan(i * radians(curveType)) * this.curveSize + noise;
           } else {
@@ -104,7 +121,7 @@ export class Scribbles {
           x = i * 1;
         }
 
-        vertex(x, y);
+        vertex(x * drawScale, y * drawScale);
       }
 
       endShape();
