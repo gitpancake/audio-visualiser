7913c424578701f738a54096101a1e7bcaa1662f
gitpancake <pye.henry@protonmail.com>
2023-07-08 18:38:35 -0400

    Checkpoint

--- src/index.js
+++ src/index.js
@@ -1,140 +1,301 @@
 import { config } from "./config";
-import { gridDivider } from "./helpers";
 import { calculateFeatures } from "./meta";
 import { Random, tokenData } from "./random";
-import { Strokes } from "./shapes";
-import { Rect } from "./shapes/rect";
 
 const R = new Random();
 const cfg = config;
-const bg = cfg.palette.background;
 
-const scaleFactor = cfg.canvasWidth / cfg.canvasHeight;
-const scaledWidth = window.innerHeight * scaleFactor;
-const scaledHeight = window.innerWidth / scaleFactor;
+/* eslint-disable */
 
-const gridWidth = cfg.canvasWidth - cfg.gridMargin.x * 2;
-const gridHeight = cfg.canvasHeight - cfg.gridMargin.y * 2;
+let audioContext;
+let frequencyData;
+let analyserNode;
+let currentHue = 0;
+let maxFrequencyTarget = 0;
+let audio;
+let progress = 0;
+
+let normalizedFreq = 0;
+
+const audioFile = "assets/audio/nova.mp3";
+
+// Draw a basic polygon, handles triangles, squares, pentagons, etc
+function polygon(x, y, radius, sides = 3, angle = 0) {
+  beginShape();
+  for (let i = 0; i < sides; i++) {
+    const a = angle + TWO_PI * (i / sides);
+    let sx = x + cos(a) * radius;
+    let sy = y + sin(a) * radius;
+
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
+
+function mousePressed() {
+  // Only initiate audio upon a user gesture
+  if (!audioContext) {
+    audioContext = new AudioContext();
+
+    // Make a stream source, i.e. MP3, microphone, etc
+    // In this case we choose an <audio> element
+    audio = document.createElement("audio");
+
+    // Upon loading the audio, let's play it
+    audio.addEventListener(
+      "canplay",
+      () => {
+        // First, ensure the context is in a resumed state
+        audioContext.resume();
+        // Now, play the audio
+        audio.play();
+      },
+      { once: true }
+    );
+
+    // Enable looping
+    audio.loop = true;
+
+    // Set source
+    audio.crossOrigin = "Anonymous";
+    audio.src = audioFile;
+
+    // Connect source into the WebAudio context
+    const source = audioContext.createMediaElementSource(audio);
+    source.connect(audioContext.destination);
+
+    analyserNode = audioContext.createAnalyser();
+
+    const detail = 4;
+    analyserNode.fftSize = 2048 * detail;
+
+    analyserNode.minDecibels = -100;
+    analyserNode.maxDecibels = -50;
+    frequencyData = new Float32Array(analyserNode.frequencyBinCount);
+
+    source.connect(analyserNode);
+  } else {
+    audio.pause();
+    audioContext.close();
+    audioContext = null;
+  }
+}
+
+// Convert the frequency in Hz to an index in the array
+function frequencyToIndex(frequencyHz, sampleRate, frequencyBinCount) {
+  const nyquist = sampleRate / 2;
+  const index = Math.round((frequencyHz / nyquist) * frequencyBinCount);
+
+  return Math.min(frequencyBinCount, Math.max(0, index));
+}
+
+// Convert an index in a array to a frequency in Hz
+function indexToFrequency(index, sampleRate, frequencyBinCount) {
+  return (index * sampleRate) / (frequencyBinCount * 2);
+}
+
+// Get the normalized audio signal (0..1) between two frequencies
+function audioSignal(analyser, frequencies, minHz, maxHz) {
+  if (!analyser) return 0;
+
+  const sampleRate = analyser.context.sampleRate;
+  const binCount = analyser.frequencyBinCount;
+
+  let start = frequencyToIndex(minHz, sampleRate, binCount);
+  const end = frequencyToIndex(maxHz, sampleRate, binCount);
+  const count = end - start;
+
+  let sum = 0;
+
+  for (; start < end; start++) {
+    sum += frequencies[start];
+  }
+
+  const minDb = analyserNode.minDecibels;
+  const maxDb = analyserNode.maxDecibels;
+
+  const valueDb = count === 0 || !isFinite(sum) ? minDb : sum / count;
+
+  return map(valueDb, minDb, maxDb, 0, 1, true);
+}
+
+// Find the frequency band that has the most peak signal
+function audioMaxFrequency(analyserNode, frequencies) {
+  let maxSignal = -Infinity;
+  let maxSignalIndex = 0;
+  for (let i = 0; i < frequencies.length; i++) {
+    const signal = frequencies[i];
+    if (signal > maxSignal) {
+      maxSignal = signal;
+      maxSignalIndex = i;
+    }
+  }
+  return indexToFrequency(maxSignalIndex, analyserNode.context.sampleRate, analyserNode.frequencyBinCount);
+}
+
+function damp(a, b, lambda, dt) {
+  return lerp(a, b, 1 - Math.exp(-lambda * dt));
+}
+
+const kandinskyPalette = [
+  [255, 0, 0], // Red
+  [0, 255, 0], // Green
+  [0, 0, 255], // Blue
+  [255, 255, 0], // Yellow
+  [255, 0, 255], // Magenta
+  [0, 255, 255], // Cyan
+  [255, 128, 0], // Orange
+  [128, 0, 255], // Purple
+  [0, 255, 128], // Light Green
+  [128, 255, 0], // Lime
+  [0, 128, 255], // Sky Blue
+  [255, 0, 128], // Pink
+];
+
+// Define color thresholds for different frequency bands
+const colorThresholds = [
+  { frequency: 100, color: [255, 0, 0] }, // Red
+  { frequency: 200, color: [0, 255, 0] }, // Green
+  { frequency: 300, color: [0, 0, 255] }, // Blue
+  { frequency: 400, color: [255, 255, 0] }, // Yellow
+  { frequency: 500, color: [255, 0, 255] }, // Magenta
+  { frequency: 600, color: [0, 255, 255] }, // Cyan
+  { frequency: 700, color: [255, 128, 0] }, // Orange
+  { frequency: 800, color: [128, 0, 255] }, // Purple
+  { frequency: 900, color: [0, 255, 128] }, // Light Green
+  { frequency: 1000, color: [128, 255, 0] }, // Lime
+  { frequency: 1100, color: [0, 128, 255] }, // Sky Blue
+  { frequency: 1200, color: [255, 0, 128] }, // Pink
+  // Add more frequency bands and colors as needed
+];
+
+let waveform, waveform_two;
+let initialSize = 0;
+const growthRate = 0.0001; // Controls the speed of growth.
+const maxLifespan = 20000; // Time (in frames) until a circle disappears.
+const count = 20000;
+const minBaseHz = 0;
+const maxBaseHz = count;
 
 // Setup Canvas
 window.setup = () => {
-  if (window.innerHeight > window.innerWidth) {
-    createCanvas(window.innerWidth, scaledHeight);
-  } else {
-    createCanvas(scaledWidth, window.innerHeight);
-  }
+  createCanvas(windowWidth, windowHeight);
 
-  noLoop();
-  noFill();
-  noStroke();
+  // Optional:
+  // If the user inserts/removes bluetooth headphones or pushes
+  // the play/pause media keys, we can use the following to ignore the action
+  navigator.mediaSession.setActionHandler("pause", () => {});
+
+  // Initialize the first circle.
+  waveform = { x: width / 2, y: height / 2, size: initialSize, lifespan: maxLifespan };
+  waveform_two = { x: width / 2, y: height / 2, size: initialSize, lifespan: maxLifespan };
 };
 
-const spacing = cfg.gridSpacing;
-const loopOneCount = cfg.isCascade ? cfg.gridSize.x : cfg.gridSize.y;
-const loopOneDiv = loopOneCount * 2;
-const loopOneDim = cfg.isCascade ? gridWidth : gridHeight;
-const loopTwoCount = cfg.isCascade ? cfg.gridSize.y : cfg.gridSize.x;
-const loopTwoDiv = loopTwoCount * 2;
-const loopTwoDim = cfg.isCascade ? gridHeight : gridWidth;
+let scaleSize = 9000;
 
-const blockDimA = gridDivider(loopOneDim / loopOneDiv, loopOneDim, loopOneCount, loopOneDim);
-
-const generateBlocks = () => {
-  let blocks = [];
-
-  for (let a = 0; a < loopOneCount; a++) {
-    const blockDimB = gridDivider(loopTwoDim / loopTwoDiv, loopTwoDim, loopTwoCount, loopTwoDim);
-
-    blocks.push([]);
-
-    for (let b = 0; b < loopTwoCount; b++) {
-      let style;
-      let blockW = cfg.isCascade ? blockDimA[a] : blockDimB[b];
-      let blockH = cfg.isCascade ? blockDimB[b] : blockDimA[a];
-
-      blockW = blockW - spacing;
-      blockH = blockH - spacing;
-
-      let pickStyle = R.random_int(2, 2);
-
-      switch (pickStyle) {
-        case 1:
-          style = new Rect(blockW, blockH, cfg.palette);
-          style.generate();
-
-          break;
-
-        case 2:
-          style = new Strokes(blockW, blockH, cfg.palette);
-          style.generate();
-
-          break;
-      }
-
-      blocks[a].push({
-        width: blockW,
-        height: blockH,
-        style,
-      });
-    }
-  }
-  return blocks;
-};
-
-const blocks = generateBlocks();
+let signalHistory = Array(25).fill(0);
+let secondSignalHistory = Array(25).fill(0);
 
 window.draw = () => {
-  background(color(bg.r, bg.g, bg.b));
-  const drawScale = width / cfg.canvasWidth;
+  // fill background
+  background("black");
 
-  const drawBlocks = (blocks) => {
-    const scaledSpacing = cfg.gridSpacing * drawScale;
-    let translateY = cfg.gridMargin.y * drawScale;
+  if (analyserNode) {
+    analyserNode.getFloatFrequencyData(frequencyData);
 
-    for (let i = 0; i < blocks.length; i++) {
-      let translateX = cfg.gridMargin.x * drawScale;
+    maxFrequencyTarget = map(audioMaxFrequency(analyserNode, frequencyData), 0, scaleSize, 0, 720, true);
+    const maxFreq = audioMaxFrequency(analyserNode, frequencyData);
 
-      for (let j = 0; j < blocks[i].length; j++) {
-        push();
+    normalizedFreq = damp(normalizedFreq, map(maxFreq, 0, scaleSize, 0, 1), 0.1, deltaTime);
+  }
 
-        if (cfg.isCascade) {
-          translate(translateY, translateX);
-        } else {
-          translate(translateX, translateY);
-        }
+  // Update and draw waveforms.
 
-        const block = blocks[i][j];
+  const maxSize = Math.min(width, height) / 32;
 
-        block.style.show(drawScale);
+  // Update size and lifespan.
+  if (waveform.size >= height) {
+    waveform.size -= Math.min(waveform.size + growthRate * normalizedFreq, maxSize); //
+  } else {
+    waveform.size += Math.min(waveform.size + growthRate * normalizedFreq, maxSize); // The waveform grows faster when the music is louder.
+  }
 
-        if (cfg.isCascade) {
-          translateX += blocks[i][j].height * drawScale + scaledSpacing;
-        } else {
-          translateX += blocks[i][j].width * drawScale + scaledSpacing;
-        }
+  // Draw waveform.
 
-        pop();
-      }
-      if (cfg.isCascade) {
-        translateY += blocks[i][0].width * drawScale + scaledSpacing;
-      } else {
-        translateY += blocks[i][0].height * drawScale + scaledSpacing;
-      }
-    }
-  };
+  beginShape();
 
-  drawBlocks(blocks);
+  const historyLength = signalHistory.length;
+
+  for (let i = 0; i < count; i++) {
+    const minHz = map(count - i, 0, count, minBaseHz, maxBaseHz);
+    const maxHz = map(count - i + 1, 0, count, minBaseHz, maxBaseHz);
+
+    let signal = analyserNode ? audioSignal(analyserNode, frequencyData, minHz, maxHz) : 0;
+
+    signalHistory[i % historyLength] = signal;
+
+    const averageSignal = signalHistory.reduce((a, b) => a + b, 0) / historyLength;
+
+    const baseSize = map(i, 0, count - 1, waveform.size, waveform.size) * normalizedFreq * 0.05;
+
+    const size = baseSize + waveform.size * averageSignal;
+
+    const angle = map(i, 0, 360, 0, PI);
+
+    const x = waveform.x + size * cos(-angle);
+    const y = waveform.y + size * sin(angle);
+
+    stroke(255, 255, 255, 45);
+
+    noFill();
+
+    vertex(x, y);
+  }
+
+  endShape(CLOSE);
+
+  // //diff freqs
+  // beginShape();
+
+  // for (let i = 0; i < count; i++) {
+  //   const minHz = map(count - i, 0, count, 0, 500);
+  //   const maxHz = map(count - i + 1, 0, count, 0, 500);
+
+  //   let signal = analyserNode ? audioSignal(analyserNode, frequencyData, minHz, maxHz) : 0;
+
+  //   secondSignalHistory[i % secondSignalHistoryLength] = signal;
+
+  //   const averageSignal = secondSignalHistory.reduce((a, b) => a + b, 0) / historyLength;
+
+  //   const baseSize = map(i, 0, count - 1, initialSize, initialSize) * normalizedFreq * 0.05;
+
+  //   const size = baseSize + initialSize * averageSignal;
+
+  //   const angle = map(i, 0, 360, 0, PI);
+
+  //   const x = waveform.x + size * cos(angle);
+  //   const y = waveform.y + size * sin(angle);
+
+  //   stroke(255, 255, 255, 10);
+  //   // vertex(x, y);
+  // }
+
+  // endShape(CLOSE);
+
+  if (!audioContext) {
+    const dim = min(width, height);
+    fill("white");
+    noStroke();
+    polygon(width / 2, height / 2, dim * 0.1, 3);
+  }
+};
+
+window.onload = () => {
+  mousePressed();
 };
 
 window.windowResized = () => {
-  const scaledHeight = window.innerWidth / scaleFactor;
-  const scaledWidth = window.innerHeight * scaleFactor;
-
-  if (window.innerHeight > window.innerWidth) {
-    resizeCanvas(window.innerWidth, scaledHeight);
-  } else {
-    resizeCanvas(scaledWidth, window.innerHeight);
-  }
+  resizeCanvas(windowWidth, windowHeight);
 };
 
 calculateFeatures(tokenData);
---
+++ src/modules/waveform.js
@@ -0,0 +1,127 @@
+export class Waveform {
+  constructor(waveform, analyserNode, frequencyData, scaleSize) {
+    this.waveform = waveform;
+    this.analyserNode = analyserNode;
+    this.frequencyData = frequencyData;
+    this.scaleSize = scaleSize;
+
+    this.normalizedFreq = 0;
+    this.maxFrequencyTarget = 0;
+    this.signalHistory = Array(25).fill(0);
+  }
+
+  damp(a, b, lambda, dt) {
+    return lerp(a, b, 1 - Math.exp(-lambda * dt));
+  }
+
+  indexToFrequency(index, sampleRate, frequencyBinCount) {
+    return (index * sampleRate) / (frequencyBinCount * 2);
+  }
+
+  audioMaxFrequency(analyserNode, frequencies) {
+    let maxSignal = -Infinity;
+    let maxSignalIndex = 0;
+    for (let i = 0; i < frequencies.length; i++) {
+      const signal = frequencies[i];
+      if (signal > maxSignal) {
+        maxSignal = signal;
+        maxSignalIndex = i;
+      }
+    }
+    return this.indexToFrequency(maxSignalIndex, analyserNode.context.sampleRate, analyserNode.frequencyBinCount);
+  }
+
+  frequencyToIndex(frequencyHz, sampleRate, frequencyBinCount) {
+    const nyquist = sampleRate / 2;
+    const index = Math.round((frequencyHz / nyquist) * frequencyBinCount);
+
+    return Math.min(frequencyBinCount, Math.max(0, index));
+  }
+
+  // Get the normalized audio signal (0..1) between two frequencies
+  audioSignal(analyser, frequencies, minHz, maxHz) {
+    if (!analyser) return 0;
+
+    const sampleRate = analyser.context.sampleRate;
+    const binCount = analyser.frequencyBinCount;
+
+    let start = this.frequencyToIndex(minHz, sampleRate, binCount);
+    const end = this.frequencyToIndex(maxHz, sampleRate, binCount);
+    const count = end - start;
+
+    let sum = 0;
+
+    for (; start < end; start++) {
+      sum += frequencies[start];
+    }
+
+    const minDb = this.analyserNode.minDecibels;
+    const maxDb = this.analyserNode.maxDecibels;
+
+    const valueDb = count === 0 || !isFinite(sum) ? minDb : sum / count;
+
+    return map(valueDb, minDb, maxDb, 0, 1, true);
+  }
+
+  draw() {
+    const growthRate = 0.0001; // Controls the speed of growth.
+    const count = 20000;
+    const minBaseHz = 0;
+    const maxBaseHz = count;
+
+    if (this.analyserNode) {
+      this.analyserNode.getFloatFrequencyData(this.frequencyData);
+
+      this.maxFrequencyTarget = map(this.audioMaxFrequency(this.analyserNode, this.frequencyData), 0, this.scaleSize, 0, 720, true);
+      const maxFreq = this.audioMaxFrequency(this.analyserNode, this.frequencyData);
+
+      this.normalizedFreq = this.damp(this.normalizedFreq, map(maxFreq, 0, this.scaleSize, 0, 1), 0.1, deltaTime);
+    }
+
+    // Update and draw waveforms.
+
+    const maxSize = Math.min(width, height) / 32;
+
+    // Update size and lifespan.
+    if (this.waveform.size >= height) {
+      this.waveform.size -= Math.min(this.waveform.size + growthRate * this.normalizedFreq, maxSize); //
+    } else {
+      this.waveform.size += Math.min(this.waveform.size + growthRate * this.normalizedFreq, maxSize); // The waveform grows faster when the music is louder.
+    }
+
+    // Draw waveform.
+
+    beginShape();
+
+    const historyLength = this.signalHistory.length;
+
+    for (let i = 0; i < count; i++) {
+      const minHz = map(count - i, 0, count, minBaseHz, maxBaseHz);
+      const maxHz = map(count - i + 1, 0, count, minBaseHz, maxBaseHz);
+
+      let signal = this.analyserNode ? this.audioSignal(this.analyserNode, this.frequencyData, minHz, maxHz) : 0;
+
+      this.signalHistory[i % historyLength] = signal;
+
+      const averageSignal = this.signalHistory.reduce((a, b) => a + b, 0) / historyLength;
+
+      const baseSize = map(i, 0, count - 1, this.waveform.size, this.waveform.size) * this.normalizedFreq * 0.05;
+
+      const size = baseSize + this.waveform.size * averageSignal;
+
+      const angle = map(i, 0, 360, 0, PI);
+
+      const x = this.waveform.x + size * cos(angle);
+      const y = this.waveform.y + size * sin(angle);
+
+      stroke(194, 165, 152, 75); // Light blue.
+      fill(194, 165, 152, 25);
+
+      vertex(x, y);
+    }
+
+    console.log(this.waveform);
+
+    endShape(CLOSE);
+  }
+}
